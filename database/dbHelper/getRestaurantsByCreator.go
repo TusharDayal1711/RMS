49aaf470f0ea8c6f0b6ad657b947747153c6984b
Tushar Dayal <tushar.dayal@remotestate.com>
2025-06-28 23:01:03 +0530

    add order by in sql query

--- database/dbHelper/getRestaurantsByCreator.go
+++ database/dbHelper/getRestaurantsByCreator.go
@@ -17,6 +17,7 @@ func GetRestaurantsByCreator(userID string) ([]models.Restaurant, error) {
 		SELECT id, name, address, longitude, latitude, created_by, created_at
 		FROM restaurants
 		WHERE created_by = $1 AND archived_at IS NULL
+		ORDER BY created_at DESC
 	`, uid)
 
 	return restaurants, err
