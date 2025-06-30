7aad84fa60cb671edee6cbec22a7e0daee5a5ddb
Tushar Dayal <tushar.dayal@remotestate.com>
2025-06-30 14:53:44 +0530

    fix mistake in query

--- database/dbHelper/removeUserSession.go
+++ database/dbHelper/removeUserSession.go
@@ -5,9 +5,9 @@ import (
 )
 
 func DeleteSessionRecord(userID string) error {
-	_, err := database.DB.DB.Exec(`
-		DELETE FROM sessions
-		WHERE user_id = $1
-	`, userID)
+	_, err := database.DB.Exec(`
+		UPDATE sessions SET archived_at = NOW()
+		WHERE user_id = $1;
+	 `, userID)
 	return err
 }
